c842f7a920e16e7399f6fc9bc0db27902066c277
jim0409 <jim.weng@prophetstor.com>
2017-11-07 20:06:10 +0800

    generate random normal data by np.random.normal(mu,std,sampleSize)

--- coursera_dp_learning/Basic_numpy/RandGenDistribution.py
+++ coursera_dp_learning/Basic_numpy/RandGenDistribution.py
@@ -1,12 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n ,p = 10 ,0.5
+# purpose : use inverse cdf to get a pair data for women and men height, weight
+# then use logistic reg to figure a parameter. Also, try with deep learning
 
-s = np.random.binomial(n,p,100)
+# how to generate random normal would refer to np.random.normal
 
-print(s)
+def checkDist(sampleVector):
+    plt.hist(sampleVector,20,normed=1, facecolor='blue', alpha=0.5)
+    mu = sampleVector.mean().round(2)
+    std = sampleVector.std().round(2)
+    plt.xlabel('Smarts')
+    plt.ylabel('Probability')
+    plt.title('Histogram of Dist: $\mu=' + str(mu) + '$, $\sigma=' + str(std) + '$')
+    plt.show()
+    return 0
 
-plt.plot(range(100),s)
-plt.show()
+# generate men height and weight with sex_term 1
+menHeight = np.random.normal(175,5,100)
+
+
+checkDist(menHeight)
+print(menHeight.mean())
+
+# generate women height and weight with sex_term 0
+
+
+
+
+
+
 
