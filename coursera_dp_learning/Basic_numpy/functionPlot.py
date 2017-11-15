ed0d56c9a39589bdabb63079cc6930b8afff8540
jim0409 <jim.weng@prophetstor.com>
2017-11-15 19:51:16 +0800

    modify

--- coursera_dp_learning/Basic_numpy/functionPlot.py
+++ coursera_dp_learning/Basic_numpy/functionPlot.py
@@ -7,7 +7,7 @@ def npSigmoid(x):
     return s
 
 # np.array => np.exp (make sure np with numpy structure as ndarray)
-x = np.array([1,2,3,4,5,6,7,8,9,10])
+x = np.array([-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10])
 y = npSigmoid(x)
 
 plt.plot(x,y)
