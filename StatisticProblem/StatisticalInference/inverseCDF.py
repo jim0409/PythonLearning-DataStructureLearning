d365c17ea150951040e6e7c8097d6ce8138eb09c
jim0409 <jim.weng@prophetstor.com>
2018-01-24 20:56:28 +0800

    test update

--- StatisticProblem/StatisticalInference/inverseCDF.py
+++ StatisticProblem/StatisticalInference/inverseCDF.py
@@ -1,6 +1,21 @@
+# http://python.jobbole.com/81321/
 import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 # random exponential
 randomSurvivalTime = np.random.exponential(scale=1)
 
 print(randomSurvivalTime)
+
+n = 10
+p = 0.3
+k = np.arange(0,21)
+binomial = stats.binom.pmf(k,n,p)
+binomial
+
+plt.plot(k,binomial,'o-')
+plt.title('Binomial: n =%i, p=%2f'%(n,p),fontsize=15)
+plt.xlabel('Number of success')
+plt.ylabel('Probability of success',fontsize=15)
+plt.show()
