3de4ae80c395d6681daee74e0757c7833dea10d9
jim0409 <jim.weng@prophetstor.com>
2018-01-25 08:16:54 +0800

    add poisson dist generate

--- StatisticProblem/StatisticalInference/probabilityDistGen/binominal.py
+++ StatisticProblem/StatisticalInference/probabilityDistGen/binominal.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 
 
-def mypltSetting(xLabelName,xFontsize,yLabelName,yFontsize,titleName):
+def mypltSetting(xLabelName='',xFontsize=10,yLabelName='',yFontsize=10,titleName='no title'):
     plt.title(titleName)
     plt.xlabel(xLabelName,fontsize=xFontsize)
     plt.ylabel(yLabelName,fontsize=yFontsize)
@@ -19,6 +19,16 @@ plt.plot(k,binomial,'o-')
 
 titleName='Binomial: n =%i, p=%2f'%(n,p)
 
-mypltSetting(xLabelName='Number of success',xFontsize=15,yLabelName='Probability of success',yFontsize=15,titleName=titleName)
+mypltSetting(xLabelName='Number of success', xFontsize=15, yLabelName='Probability of success', yFontsize=15, titleName=titleName)
 
 plt.show()
+
+# # .rvs函數模擬返回10000個參數為n,p的二項隨機變數
+# binom_sim = stats.binom.rvs(n=10, p=0.3, size =10000)
+#
+# print("Mean :%g" % np.mean(binom_sim))
+# print("SD: %g" % np.std(binom_sim, ddof=1))
+# plt.hist(binom_sim, bins=10, normed=True) #normed
+#
+# mypltSetting(xLabelName='x',yLabelName='density')
+# plt.show()
---
+++ StatisticProblem/StatisticalInference/probabilityDistGen/poisson.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+rate =2
+n = np.arange(0,10)
+y = stats.poisson.pmf(n,rate)
+print(y)
+
+plt.plot(n,y)
+plt.show()
+
+# # 使用rvs作圖
+# data = stats.poisson.rvs(mu=2, loc=0, size=1000)
+# print("Mean: %g" % np.mean(data))
+# print("SD: %g" % np.std(data, ddof=1))
+#
+# # 製作空圖
+# plt.figure()
+#
+# plt.hist(data, bins=9, normed=True)
+# plt.xlim(0,10)
+# plt.xlabel("Number of accidents")
+# plt.title("Simulating Poisson Random Variables")
+# plt.show()
