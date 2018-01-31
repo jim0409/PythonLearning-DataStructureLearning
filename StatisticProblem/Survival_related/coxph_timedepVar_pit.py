ec06023f546961a9a04f0428c87471b739a8e2a6
jim0409 <jim.weng@prophetstor.com>
2018-01-31 23:02:34 +0800

    commit nothing

--- StatisticProblem/Survival_related/coxph_timedepVar_pit.py
+++ StatisticProblem/Survival_related/coxph_timedepVar_pit.py
@@ -10,8 +10,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lifelines
 
+
+# will need sample_size to know how many simulation data needed
+# censor_rate to make sure not too much data censor
+# intercept means the base risk of each vairable
+# slop terms for the increasing rate of risk
+# beta and landa is the same define as coxph
+# cov_type(time dep) default is linear
 class dataInfo():
-    def __init__(self, sample_size, censor_rate=0, intercept=0, slope=0, beta=0, landa=0 , cov_type="linear"):
+    def __init__(self, sample_size, censor_rate=0, intercept=0, slope=0, beta=0, landa=0, cov_type="linear"):
         self.sample_size = sample_size
         self.censor_rate = censor_rate
         self.intercept = intercept
@@ -28,17 +35,17 @@ class dataInfo():
         return event_T
 
 
-def inversecdf(intercet, slope, beta, landa, data, cov_type="linear" ,error_term=False):
-    simulate_data = np.log( 1 + slope * beta * np.array(data) * np.exp( -intercet * beta ) / landa )/( slope * beta)
+def inversecdf(intercet, slope, beta, landa, data, cov_type="linear", error_term=False):
+    simulate_data = np.log(1 + slope * beta * np.array(data) * np.exp(-intercet * beta) / landa) / (slope * beta)
     return simulate_data
 
 
 if __name__ == '__main__':
-
     newData = dataInfo(sample_size=1000, censor_rate=1, intercept=1, slope=1, beta=1, landa=1)
     data = [1, 2, 3, 4, 5]
 
-    result = inversecdf(intercet=newData.intercept, slope=newData.slope, beta=newData.beta, landa=newData.landa , data=data)
+    result = inversecdf(intercet=newData.intercept, slope=newData.slope, beta=newData.beta, landa=newData.landa,
+                        data=data)
 
     plt.hist(newData.genTimeData(), normed=1, facecolor="green", alpha=0.5)
     plt.show()
