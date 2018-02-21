17d59569f67dbb9b2593eec08911005b1475ef0a
jim0409 <jim.weng@prophetstor.com>
2018-02-22 00:06:34 +0800

    update

--- StatisticProblem/lifelinesSampleCode/stratifiation.py
+++ StatisticProblem/lifelinesSampleCode/stratifiation.py
@@ -1,3 +1,10 @@
+from matplotlib.pylab import plt
+from lifelines.datasets import load_rossi
+from lifelines import CoxPHFitter
+
+rossi_dataset = load_rossi()
+
+cph = CoxPHFitter()
 
 # stratification
 cph.fit(rossi_dataset, 'week', event_col='arrest', strata=['race'])
