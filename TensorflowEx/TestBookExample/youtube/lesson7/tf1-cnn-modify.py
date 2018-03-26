931fdad3c03efd373cf440711f9acb50c4552cee
jim0409 <jim.weng@prophetstor.com>
2018-03-26 23:00:37 +0800

    lesson7 tf1-cnn-modify

--- TensorflowEx/TestBookExample/youtube/lesson7/tf1-cnn-modify.py
+++ TensorflowEx/TestBookExample/youtube/lesson7/tf1-cnn-modify.py
@@ -1,5 +1,3 @@
-# would need to add name_scope and statistic related graph
-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -10,6 +8,19 @@ batch_size = 100
 
 # 計算一共有多少個批次
 n_batch = mnist.train.num_examples // batch_size
+
+
+# 參數概要
+def variable_summarize(var):
+    with tf.name_scope('summarize'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)  # 平均值
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))  # 最大值
+        tf.summary.scalar('min', tf.reduce_min(var))  # 最小值
+        tf.summary.histogram('histogram', var)  # 直方圖
 
 
 # 初始化權值
@@ -39,68 +50,114 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-# 定義兩個placeholder
-x = tf.placeholder(tf.float32, [None, 784])  # 28x28
-y = tf.placeholder(tf.float32, [None, 10])
+# 定義命名空間
+with tf.name_scope('input'):
+    # 定義兩個placeholder
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')  # 28x28
+    y = tf.placeholder(tf.float32, [None, 10], name='y-input')
+    with tf.name_scope('x_image'):
+        # 改變x的格式轉為4D的向量[batch, in_height, in_width, in_channels]
+        x_image = tf.reshape(x, [-1, 28, 28, 1], name='x_image')
 
-# 改變x的格式轉為4D的向量[batch, in_height, in_width, in_channels]
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+with tf.name_scope('Conv1'):
+    # 初始化第一個卷積層的權值和偏量
+    with tf.name_scope('W_conv1'):
+        W_conv1 = weight_variable([5, 5, 1, 32], name='W_conv1')  # 採用5x5的採樣窗口，32個卷積核從1個平面抽取特徵
+    with tf.name_scope('b_conv1'):
+        b_conv1 = bias_variable([32], name='b_conv1')  # 每一個卷積核一個偏量
 
-# 初始化第一個卷積層的權值和偏量
-W_conv1 = weight_variable([5, 5, 1, 32])  # 採用5x5的採樣窗口，32個卷積核從1個平面抽取特徵
-b_conv1 = bias_variable([32])  # 每一個卷積核一個偏量
+    # 把x_image和權值向量進行卷積，再加上偏量值，然後應用於relu激活函數
+    with tf.name_scope('h_conv1'):
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1, name='h_conv1')
+    with tf.name_scope('h_pool1'):
+        h_pool1 = max_pool_2x2(h_conv1, name='h_pool1')  # 執行max-pooling
 
-# 把x_image和權值向量進行卷積，再加上偏量值，然後應用於relu激活函數
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)  # 執行max-pooling
+with tf.name_scope('Conv2'):
+    # 初始化第二個卷積層的權值和偏量
+    with tf.name_scope('W_conv2'):
+        W_conv2 = weight_variable([5, 5, 32, 64], name='W_conv2')  # 採用5x5的採樣窗口，64個卷積核從32個平面抽取特徵
+    with tf.name_scope('b_conv2'):
+        b_conv2 = bias_variable([64], name='b_conv2')  # 每一個卷積核一個偏量
 
-# 初始化第二個卷積層的權值和偏量
-W_conv2 = weight_variable([5, 5, 32, 64])  # 採用5x5的採樣窗口，64個卷積核從32個平面抽取特徵
-b_conv2 = bias_variable([64])  # 每一個卷積核一個偏量
+    # 把x_image和權值向量進行卷積，再加上偏量值，然後應用於relu激活函數
+    with tf.name_scope('h_conv2'):
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2, name='h_conv2')
+        h_pool2 = max_pool_2x2(h_conv2, name='h_pool2')  # 執行max-pooling
 
-# 把x_image和權值向量進行卷積，再加上偏量值，然後應用於relu激活函數
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)  # 執行max-pooling
+    # 28x28 的圖片第一次卷積後還是28x28，第一次池化後變為14x14
+    # 第二次卷積後為14x14，第二次池化後變為7x7
+    # 透過上面操作後得到64張7x7的平面
+    # 初始化第一個全聯接層的權值
+    with tf.name_scope('w_fc1'):
+        W_fc1 = weight_variable([7 * 7 * 64, 1024], name='w_fc1')  # 上一張有7*7*64個神經元，全連結層有1024個神經元
+    with tf.name_scope('b_fc1'):
+        b_fc1 = bias_variable([1024], name='b_fc1')  # 1024個節點
 
-# 28x28 的圖片第一次卷積後還是28x28，第一次池化後變為14x14
-# 第二次卷積後為14x14，第二次池化後變為7x7
-# 透過上面操作後得到64張7x7的平面
+    # 把池化層2的輸出層平化為1維
+    with tf.name_scope('h_pool2_flat'):
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64], name='h_pool2_flat')
+    # 求第一個全連接層的輸出
+    with tf.name_scope('h_fc1'):
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name='h_fc1')
 
-# 初始化第一個全聯接層的權值
-W_fc1 = weight_variable([7 * 7 * 64, 1024])  # 上一張有7*7*64個神經元，全連結層有1024個神經元
-b_fc1 = bias_variable([1024])  # 1024個節點
+    # keep_prob用來表示神經元的輸出機率
+    with tf.name_scope('keep_prob'):
+        keep_prob = tf.placeholder(tf.float32)
+    with tf.name_scope('h_fc1_drop'):
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-# 把池化層2的輸出層平化為1維
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-# 求第一個全連接層的輸出
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# keep_prob用來表示神經元的輸出機率
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# 初始化第二個全聯接層的權值
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-# 計算輸出
-prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+with tf.name_scope('fc2'):
+    # 初始化第二個全聯接層的權值
+    with tf.name_scope('W_fc2'):
+        W_fc2 = weight_variable([1024, 10], name='W_fc2')
+    with tf.name_scope('b_fc2'):
+        b_fc2 = bias_variable([10], name='b_fc2')
+    with tf.name_scope('wx_plus_b2'):
+        wx_plus_b2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    # 計算輸出
+    with tf.name_scope('softmax'):
+        prediction = tf.nn.softmax(wx_plus_b2)
 
 # 交叉熵代價函數
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=prediction))
+with tf.name_scope('cross_entropy'):
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=prediction))
+    tf.summary.scalar('cross_entorpy', cross_entropy)
+
 # 使用AdamOptimizer進行優化
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-# 結果存放在一個布爾列表中
-correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))  # argmax返回一維張亮中最大的值所在的位置
+with tf.name_scope('train'):
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
 # 求準確率
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+with tf.name_scope('accuracy'):
+    # 結果存放在一個布爾列表中
+    with tf.name_scope('correct_prediction'):
+        # 求準確率
+        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))  # argmax返回一維張亮中最大的值所在的位置
+    with tf.name_scope('accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# 合併所有的summary
+merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for epoch in range(21):
-        for batch in range(n_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 0.7})
+    train_writer = tf.summary.FileWriter('logs/train', sess.graph)
+    test_writer = tf.summary.FileWriter('logs/test', sess.graph)
+    # 訓練模型
+    for i in range(21):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 0.5})
+        # 紀錄訓練集計算的參數
+        summary = sess.run(merged, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.0})
+        train_writer.add_summary(summary, i)
+        # 紀錄測試集計算的參數
+        batch_xs, batch_ys = mnist.test.next_batch(batch_size)
+        summary = sess.run(merged, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.0})
+        # sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 0.7})
+        test_writer.add_summary(summary, i)
 
-        acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0})
-        print("Iter " + str(epoch) + ". Testing Accuracy= " + str(acc))
+    if i % 10 == 0:
+        test_acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0})
+        train_acc = sess.run(accuracy,
+                             feed_dict={x: mnist.train.images[:10000], y: mnist.train.labels[:10000], keep_prob: 1.0})
+        print("Iter " + str(i) + ". Testing Accuracy= " + str(test_acc) + 'Training Accuracy=' + str(train_acc))
