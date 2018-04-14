4f8dda2e5d10896fcb14eb7831d6e43cb4a2291f
jim0409 <jim.weng@prophetstor.com>
2018-04-15 07:44:11 +0800

    modfiy structure

--- StatisticProblem/project/simulation/src/main.py
+++ StatisticProblem/project/simulation/src/main.py
@@ -8,12 +8,22 @@ import sys
 import configparser
 import argparse
 import logging
+from datetime import datetime
+# import StatisticProblem.project.simulation.gen.rand_dist.linear as LinearModel
+from StatisticProblem.project.simulation.gen.rand_dist.linear import LinearSimulation
+# from StatisticProblem.project.simulation.db.influxdb.db_writer.influxdb_writer import DBwriter
 
 sample_config = """
-[TestConf]
-Distribution = "exponential"
-Parameters = 1
+[FitModel]
+Model = "Linear"
+number_of_points = 500
+
+[Linear-Parameters]
+slope = 0.22
+intercept = 0.78
 """
+
+current_time = datetime(2018, 4, 14)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -37,14 +47,20 @@ else:
     else:
         config = configparser.ConfigParser()
         config.read(opts.config)
-        configFile = config['TestConf']
-        conf1 = configFile.get('Distribution')
-        conf2 = configFile.getfloat('Parameters')
+        configFile = config['FitModel']
+        conf1 = configFile.get('Model')
         if conf1 is not None:
-            if conf1.split('"')[1] != "Exponential":
-                logging.info("only support Exponential now!")
+            if conf1.split('"')[1] != "Linear":
+                logging.info("only support Linear model now!")
                 sys.exit(1)
             else:
-                logging.info("Use distribution " + conf1 + " to generate data")
+                conf2 = config['Linear-Parameters']
+                logging.info("Use " + conf1 + " model to generate data")
                 if conf2 is not None:
-                    logging.info("with parameter scale %d" % conf2)
+                    slope = conf2.getfloat('slope')
+                    intercept = conf2.getfloat('intercept')
+                    logging.info(
+                        "with parameter slope %0.3f and intercept %0.3f" % (slope,intercept))
+                    linear_fit_model = LinearSimulation(time=current_time, a=slope, b=intercept)
+                    # linear_fit_model.estimator_ai()
+                    # linear_fit_model.writedb()
