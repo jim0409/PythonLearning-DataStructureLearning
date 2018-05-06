70a2159f12bbb56d6fc747e6b7d2bd48ccf57525
jim0409 <berserker.01.tw@hotmail.com>
2018-05-06 21:56:54 +0800

    modfiy trainning model

--- tbrain/src/train_model.py
+++ tbrain/src/train_model.py
@@ -6,36 +6,34 @@ import numpy as np
 import copy
 
 BATCH_START = 0  # 定義batch開始處
-TIME_STEPS = 5  # 每一層有幾個ＲＮＮ - 定義5個工作天一層
-BATCH_SIZE = 5  # 定義每次batch提出的量的大小
+TIME_STEPS = 10  # 每一層有幾個ＲＮＮ - 定義5個工作天一層
+BATCH_SIZE = 35  # 定義每次batch提出的量的大小
 INPUT_SIZE = 1  # 放入參數個數
 OUTPUT_SIZE = 1  # 輸出參數個數
 CELL_SIZE = 10  # 多少個hidden units
 LEARNING_RATE = 0.006  # 學習率
 
 Df = read_tbrain_data('../data/taetfp.csv')  # 50 51 52
-final_pred_array = []
-
 # 使用code 50的data
 trainDf = Df[(Df.code == 50)]
 
+# declaration global variable
+final_pred_array = []
+final_average = 0
+final_std = 0
 
-def normalization(data):
-    data_average = np.average(data)
-    data_std = np.std(data)
-    z_data = (data-data_average)/data_std
-    return z_data
 
 # normalization
-# close
-close_avg = np.average(trainDf.close)
-close_std = np.std(trainDf.close)
-# trainDf.close = (trainDf.close - close_avg) / close_std
+def normalization(data):
+    global final_average, final_std
+    data_average = np.average(data)
+    data_std = np.std(data)
+    z_data = (data - data_average) / data_std
 
-# open
-open_avg = np.average(trainDf.open)
-open_std = np.std(trainDf.open)
-# trainDf.open = (trainDf.open - open_avg) / open_std
+    final_average = data_average
+    final_std = data_std
+
+    return z_data
 
 
 def get_batch():
@@ -43,10 +41,11 @@ def get_batch():
     start_pt = BATCH_START
     end_pt = BATCH_START + TIME_STEPS * BATCH_SIZE
     xs = np.array(copy.copy(trainDf.date[start_pt:end_pt]).reshape(BATCH_SIZE, TIME_STEPS))
-    seq = np.array(copy.copy(trainDf.close[start_pt:end_pt]).reshape(BATCH_SIZE, TIME_STEPS))
-    seq = normalization(seq)
     res = np.array(copy.copy(trainDf.open[1 + start_pt:1 + end_pt]).reshape(BATCH_SIZE, TIME_STEPS))
     res = normalization(res)
+    seq = np.array(copy.copy(trainDf.close[start_pt:end_pt]).reshape(BATCH_SIZE, TIME_STEPS))
+    seq = normalization(seq)
+
     BATCH_START += TIME_STEPS
     return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
 
@@ -65,7 +64,7 @@ if __name__ == '__main__':
 
     # 根據data量做調整，目前code 50有data 13XX
     # 能做迭代次數為 13XX/ (batch_size*step_size)
-    for i in range(52):
+    for i in range(96):
         seq, res, xs = get_batch()
         if i == 0:
             feed_dict = {
@@ -84,8 +83,7 @@ if __name__ == '__main__':
             [model.train_op, model.cost, model.cell_final_state, model.pred],
             feed_dict=feed_dict)
 
-        print(i)
-        print('cost: ', round(cost, 4))
+        print('the %d_th cost: %.3f' % (i, round(cost, 4)))
         result = sess.run(merged, feed_dict)
 
         # plotting
@@ -93,12 +91,14 @@ if __name__ == '__main__':
         # plt.ylim((-1.2, 1.2))
         plt.draw()
         plt.pause(.3)
+    print(xs[-1:])
     final_pred_array = pred
-
+    final_res_array = res
 
 # 打印出平均數以及標準差
-print("the close average is %.3f" % close_avg)
-print("the close std is %.3f" % close_std)
-print("the open average is %.3f" % open_avg)
-print("the open std is %.3f" % open_std)
-print(final_pred_array.flatten())
+print("the final_close average is %.3f" % final_average)
+print("the final_close std is %.3f" % final_std)
+
+print("the final pred_array is ", final_pred_array.flatten()[-TIME_STEPS*2:])
+print("the final pred result is ", final_res_array.flatten()[-TIME_STEPS*2:])
+
