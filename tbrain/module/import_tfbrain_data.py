3275e03b51886763c62caf86084b3905e8164f74
jim0409 <berserker.01.tw@hotmail.com>
2018-05-06 17:19:04 +0800

    rename some package

---
+++ tbrain/module/import_tfbrain_data.py
@@ -0,0 +1,50 @@
+# -- coding: utf-8 --
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+# translate timestamp for matplotlib to plot
+def trans_data_time(raw_data_times):
+    data_times = []
+    for ti in raw_data_times:
+        ti = str(ti)
+        year = int(ti[:4])
+        month = int(ti[4:6])
+        day = int(ti[6:8])
+        data_time = datetime(year=year, month=month, day=day)
+        data_times.append(data_time)
+    return data_times
+
+
+# rename the database cols' name
+def data_pre_process(raw_data):
+    # since the second col is needless chinese code
+    raw_data.columns = ['code', 'date', 'c_code', 'open', 'max', 'min', 'close', 'deal']
+    data = raw_data.drop(['c_code'], axis=1)
+    data.iloc[:, 1] = trans_data_time(data.iloc[:, 1])
+    return data
+
+
+def read_tbrain_data(csv_data):
+    raw_csv_ata = pd.read_csv(csv_data, encoding="ISO-8859-1")  # encoding with 'big5-hkscs' or 'ISO-88591-1'
+    used_data = data_pre_process(raw_csv_ata)  # clean data
+    return used_data
+
+
+Df = read_tbrain_data('../data/taetfp.csv')  # 50 51 52
+# Df = readData('../data/tasharep.csv')
+# Df = readData('../data/tetfp.csv')
+# Df = readData('../data/tsharep.csv')
+
+# 0050/0051/0052
+
+
+# 使用code 50的data
+testdf = Df[(Df.code == 50)]
+# check test dataframe code
+print(testdf.head())
+print(testdf.tail())
+
+plt.plot(testdf.date, testdf.close)
+plt.show()
