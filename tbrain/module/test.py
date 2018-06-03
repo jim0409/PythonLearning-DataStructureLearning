a04b4f268c13b5656fae34ad33435bd161f8ee9a
Jim Weng <berserker.01.tw@hotmail.com>
2018-06-03 14:32:44 +0800

    Update test.py

--- tbrain/module/test.py
+++ tbrain/module/test.py
@@ -1,6 +1,10 @@
 import pickle
+import csv
 
 with open('pickle_example.pickle', 'rb') as file:
     test_dic = pickle.load(file)
 
-print(test_dic)
+with open('some.csv','w',newline='') as f:
+    for i in test_dic.keys():
+        writer = csv.writer(f)
+        writer.writerows(test_dic[i])
