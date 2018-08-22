7164b9775aa7c1883306498680ee2175c514dd5e
jim0409 <berserker.01.tw@hotmail.com>
2018-08-22 21:56:23 +0800

    improve kwargs code

--- BasicLangLearning/*args&**kwargs/main.py
+++ BasicLangLearning/*args&**kwargs/main.py
@@ -1,12 +1,31 @@
 a = {1: 1, 2: 2, 3: 3, 4: 4}
 
+b = {'a1': 1, 'a2': 2}
+
+test_enum = {
+    'a1': 1,
+    'a2': 2
+}
+
 
 def test(*args):
     return args
 
+
 def test2(**kwargs):
-    return kwargs
+    for i, j in kwargs.items():
+        print i, j
+    return 0
 
-print(test(a))
 
-# print(test2(a))
+# print(test(a))
+print test(b)
+
+print("-----")
+test2(key=a, value=b, index=b)
+print("-----")
+test2(**b)
+print("-----")
+test2(**test_enum)
+# print("-----")
+# test2(**a) # would return error since keywords must be strings
