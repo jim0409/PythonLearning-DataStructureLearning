db26feccee6be159eecd1d07a931d8b5d736a4fc
jim0409 <berserker.01.tw@hotmail.com>
2018-09-02 11:47:44 +0800

    isinstance method

---
+++ BasicLangLearning/package_tutorial/isinstance/isinstance.py
@@ -0,0 +1,13 @@
+a = {'a':'test','b':123}
+
+print type(a)
+
+
+# check a's type is list or not
+print isinstance(a,list)
+
+# check a's type is dict or not
+print isinstance(a,dict)
+
+# check a's type is type(a) or not
+print isinstance(a,type(a))
