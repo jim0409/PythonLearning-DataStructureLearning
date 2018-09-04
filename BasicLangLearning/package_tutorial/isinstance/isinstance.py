8a734cc9af8509d1b6eb71e360ff0a6ed05db881
jim0409 <berserker.01.tw@hotmail.com>
2018-09-04 08:32:22 +0800

    isinstance example

--- BasicLangLearning/package_tutorial/isinstance/isinstance.py
+++ BasicLangLearning/package_tutorial/isinstance/isinstance.py
@@ -1,13 +1,12 @@
-a = {'a':'test','b':123}
+a = {'a': 'test', 'b': 123}
 
 print type(a)
 
-
 # check a's type is list or not
-print isinstance(a,list)
+print isinstance(a, list)
 
 # check a's type is dict or not
-print isinstance(a,dict)
+print isinstance(a, dict)
 
 # check a's type is type(a) or not
-print isinstance(a,type(a))
+print isinstance(a, type(a))
