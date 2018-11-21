45cf61abd82efaaade479ceff707a519ba146e19
jim0409 <berserker.01.tw@hotmail.com>
2018-11-21 23:44:17 +0800

    modify ansible part

--- ansible-playbook/subprocess-ansible/main.py
+++ ansible-playbook/subprocess-ansible/main.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-cmd = "ansible-playbook -i inventory/inventory testplaybook.yml"
+cmd = "ansible-playbook -i inventory/inventory testplaybook.yml -v"
 # my_env = os.environ.copy()
 # my_env["TOKEN"] = _get_token()
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
