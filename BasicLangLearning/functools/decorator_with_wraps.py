f44bbec39b26320bd92f79d23380f1fb4d156598
jim0409 <berserker.01.tw@hotmail.com>
2019-01-05 13:52:02 +0800

    rename decorator___wraps practice

--- BasicLangLearning/functools/decorator_with_wraps.py
+++ BasicLangLearning/functools/decorator_with_wraps.py
@@ -1,9 +1,12 @@
+import functools
+
 def logged(func):
+    @functools.wraps(func)
     def with_logging(*args, **kwargs):
         """
         test with_logging
         """
-        print(func.__name__+" was called ...")
+        print(func.__name__ + " was called")
         return func(*args, **kwargs)
     return with_logging
 
@@ -16,3 +19,12 @@ def f(x):
 
 print("the f.__name__ is "+f.__name__)
 print("the f.__doc__ is "+f.__doc__)
+
+# 雖然使用裝飾法，但是有加functools.wraps所以f.__name__跟f.__doc__會返回函數的name及doc
+# f.__name__ 為 f
+# f.__doc__ 為 test func f
+
+print(f(2))
+
+# f was called ...
+# 6
--- BasicLangLearning/functools/decorator_without_wraps.py
+++ BasicLangLearning/functools/decorator_without_wraps.py
@@ -1,12 +1,9 @@
-import functools
-
 def logged(func):
-    @functools.wraps(func)
     def with_logging(*args, **kwargs):
         """
         test with_logging
         """
-        print(func.__name__ + " was called")
+        print(func.__name__+" was called ...")
         return func(*args, **kwargs)
     return with_logging
 
@@ -19,3 +16,12 @@ def f(x):
 
 print("the f.__name__ is "+f.__name__)
 print("the f.__doc__ is "+f.__doc__)
+
+# 因為使用裝飾法，所以f.__name__跟f.__doc__會返回裝飾法的name及doc
+# f.__name__ 為 with_logging
+# f.__doc__ 為 test_with_logging
+
+print(f(2))
+
+# f was called ...
+# 6
