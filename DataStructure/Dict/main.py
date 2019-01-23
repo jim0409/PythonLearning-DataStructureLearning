5e47b696512459773de70bc95a13e768e63547d0
jim0409 <berserker.01.tw@hotmail.com>
2019-01-23 13:49:40 +0800

    dict practice

---
+++ DataStructure/Dict/main.py
@@ -0,0 +1,77 @@
+class field_key(object):
+    basic_compare_list = [
+		'EQUAL',
+		'NOT_EQUAL',
+	]
+
+    PKT_compare_list = basic_compare_list + ['OVER', 'UNDER']
+    TCP_FLAG_compare_list = basic_compare_list + ['AND', 'OR']
+
+    value_list = {
+		'ipv4': basic_compare_list,
+		'ipv6': basic_compare_list,
+		'mac': basic_compare_list,
+		'CIDR': basic_compare_list,
+		'port': basic_compare_list,
+		'TCP_UDP': basic_compare_list,
+		'PKT_LEN': PKT_compare_list,
+		'PKT_VOLUME': PKT_compare_list,
+		'TCP_FLAGS': TCP_FLAG_compare_list,
+		'GSLB': basic_compare_list,
+		'ASN': basic_compare_list,
+		'COUNTRY': basic_compare_list,
+		'REGION': basic_compare_list,
+		'SUB_REGION': basic_compare_list,
+		'ME_ID': basic_compare_list,
+		'FILTER_ID': basic_compare_list,
+		'ME_RESULT': basic_compare_list,
+	}
+
+    field_list = {
+		'LOCAL_IP': {'ipv4', 'ipv6', 'mac'},
+		'REMOTE_IP': {'ipv4', 'ipv6', 'mac'},
+		'NEXT_HOP': {'ipv4', 'ipv6', 'mac'},
+		'LOCAL_CIDR_A/B/C/D': {'CIDR'},
+		'REMOTE_CIDR_A/B/C/D': {'CIDR'},
+		'IP_VER': {'ipv4', 'ipv6'},
+		'LOCAL_PORT': {'port'},
+		'REMOTE_PORT': {'port'},
+		'NAT_PORT': {'port'},
+		'NIC_PORT': {'port'},
+		'PROTOCOL': {'TCP_UDP'},
+		'PKT_LEN': {'PKT_LEN'},
+		'PKT_VOLUME': {'PKE_VOLUME'},
+		'TCP_FLAGS': {'TCP_FLAGS'},
+		'LOCAL_GSLB': {'GSLB'},
+		'REMOTE_GSLB': {'GSLB'},
+		'LOCAL_ASN': {'ASN'},
+		'REMOTE_ASN': {'ASN'},
+		'LOCAL_COUNTRY': {'COUNTRY'},
+		'REMOTE_COUNTRY': {'COUNTRY'},
+		'LOCAL_REGION': {'REGION'},
+		'REMOTE_REGION': {'REGION'},
+		'LOCAL_SUB_REG': {'SUB_RESGION'},
+		'REMOTE_SUB_REG': {'SUB_REGION'},
+		'ME_ID': {'ME_ID'},  # not for field rule
+		'FILTER_ID': {'FILTER_ID'},  # not for field rule
+		'ME_RESULT': {'ME_RESULT'},
+	}
+
+    @classmethod
+    def key_to_dict(cls, key):
+    	return {i: cls.value_list[i] for i in cls.field_list[key]}
+
+
+a = field_key()
+
+print(a.field_list['LOCAL_IP'])
+
+print(a.field_list['REMOTE_IP'])
+
+print(field_key.key_to_dict('LOCAL_IP'))
+
+# def key_to_dict(key):
+#     a = field_key()
+#     return {i: a.value_list[i] for i in a.field_list[key]}
+
+# print(key_to_dict('LOCAL_IP'))
