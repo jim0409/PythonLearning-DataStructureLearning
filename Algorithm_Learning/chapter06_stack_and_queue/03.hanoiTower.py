e594d2118d7a395abc2f1aa1eb440ab50b30459e
jim0409 <berserker.01.tw@hotmail.com>
2019-04-06 22:59:50 +0800

    add note for hanoiTower explain

--- Algorithm_Learning/chapter06_stack_and_queue/03.hanoiTower.py
+++ Algorithm_Learning/chapter06_stack_and_queue/03.hanoiTower.py
@@ -10,3 +10,30 @@ def hanoi(n, p1, p2, p3):
 
 j = int(input('please enter the total ring want to move: '))
 hanoi(j, 1, 2, 3)
+
+'''
+以n=3為例
+step 1.
+[ 123| | ]
+
+step 2.
+[ 23| | 1]
+
+step 3.
+[ 3| 2| 1]
+
+step 4.
+[ 3| 12| ]
+
+step 5.
+[ | 12| 3]
+
+step 6.
+[ 1| 2| 3]
+
+step 7.
+[ 1| | 23]
+
+step 8.
+[ | | 123]
+'''
