ee9be36d3cda59339c75886f5217ed1ff1bf141b
jim0409 <berserker.01.tw@hotmail.com>
2019-04-11 17:37:27 +0800

    add comment for btree

--- Algorithm_Learning/chapter07_tree_algorithm/practice/funTree.py
+++ Algorithm_Learning/chapter07_tree_algorithm/practice/funTree.py
@@ -1,14 +1,16 @@
 def Btree_create(tree_deep, data):
-    btree = [0]*pow(2, tree_deep)
-    for i in range(1, len(data)):
-        level = 1
-        while btree[level] is not 0:
-            if data[i] > btree[level]:
-                level = level*2+1
+    btree = [0]*pow(2, tree_deep)       # 因為deep是4，所以產生2^4個0在一維array作為btree，內容:[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for i in range(1, len(data)):       # 從data的第一個值開始讀取到最後一個值
+        level = 1                       # 設定一個level常數，控制deep的層級
+
+        while btree[level] is not 0:    # 當該層級的btree不是0的時候執行以下;0則直接賦予該位置值 ==> btree[level]= data[i]
+            if data[i] > btree[level]:  # 1.判斷data對應值是否大於該內容的值 
+                level = level*2+1       # 2.是的話就將該值放在右邊(level*2+1)，否則放在左邊(level)
             else:
                 level = level*2
+        btree[level] = data[i]          # 賦予該btree[level]值
 
-        btree[level] = data[i]
     return btree
 
 def show_tree(tree, tree_deep):
