9c820ec2b934b7fe95dbe9a68b3cdd03b884ab2f
jim0409 <berserker.01.tw@hotmail.com>
2019-04-21 11:58:01 +0800

    add debug print and modify word from top to vertex

--- Algorithm_Learning/chapter08_graph_algorithm/01.deep_wide_search.py
+++ Algorithm_Learning/chapter08_graph_algorithm/01.deep_wide_search.py
@@ -14,7 +14,7 @@ def dfs(current):  # deep priority func
     print('[%d]' % current, end='')
     ptr = head[current].next
     while ptr != None:
-        if run[ptr.val] == 0:  # if ptr doesn't reach top
+        if run[ptr.val] == 0:  # if ptr doesn't reach vertex
             dfs(ptr.val)  # recursive func
         ptr = ptr.next
 
@@ -33,33 +33,37 @@ data = [[1, 2], [2, 1], [1, 3], [3, 1], [2, 4], [4, 2], [2, 5], [5, 2], [3, 6], 
         -- 7 --|
 """
 
-for i in range(1, 9):  # set 8 top point
+for i in range(1, 9):  # set 8 vertex point
     run[i] = 0  # set every point as unreachable
     head[i] = list_node()
     head[i].val = i  # init every point's value
     head[i].next = None
     ptr = head[i]  # set ptr as head
     for j in range(20):  # calculate 20 edge line
-        if data[j][0] == i:  # if start_pt is equal to the hed, concatenate it with top queue
+        print("start loop with (i, j) in ({}, {})".format(i, j))
+        if data[j][0] == i:  # if start_pt is equal to the hed, concatenate it with vertex queue
             newnode = list_node()
             newnode.val = data[j][1]
             newnode.next = None
             while True:
+                print("add new node to head[{}] with value {}".format(
+                    i, data[j][1]))
                 ptr.next = newnode  # add new node
                 ptr = ptr.next
                 if ptr.next == None:
                     break
 
-print('the edge queue within the graph:')  # print out the graph within the graph
+# print out the graph within the graph
+print('the edge queue within the graph:')
 for i in range(1, 9):
     ptr = head[i]
-    print('top %d =>' % ptr.val, end='')
-    
+    print('vertext %d =>' % ptr.val, end='')
+
     # 不打印出節點，只打印出該節點能夠遍佈的點
     while ptr.next != None:
         print('[%d]' % ptr.next.val, end='')
         ptr = ptr.next
     print()
-print('move to top from deep')  # print out the path
+print('move to vertextt from deep')  # print out the path
 dfs(1)
 print()
