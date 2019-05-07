7a33a108a2aba446acc9960022a3e649d32d4c74
jim0409 <berserker.01.tw@hotmail.com>
2019-05-07 10:47:23 +0800

    refactor directory

--- StructurePractice/module1/func1.py
+++
@@ -1,4 +0,0 @@
-from __future__ import print_function
-
-def BeImportFunc(a,b,c=0):
-    return a+b+c
--- StructurePractice/src/main.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from StructurePractice.module2.class1 import BeImportedClass
-
-test = BeImportedClass(1)
-test.printC1()
-
-test.upgradeC1()
-test.printC1()
